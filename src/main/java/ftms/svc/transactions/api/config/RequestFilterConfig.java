9850ac92259a24494a3211fb2b5aa818693dcd83
Vitthal Choudhari <112979693+vitthal1001@users.noreply.github.com>
2025-11-09 15:03:51 +0530

    feat(transaction): -Add H2 sanity check test

--- src/main/java/ftms/svc/transactions/api/config/RequestFilterConfig.java
+++ src/main/java/ftms/svc/transactions/api/config/RequestFilterConfig.java
@@ -4,11 +4,13 @@ import ftms.svc.transactions.api.filter.RequestValidationFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class RequestFilterConfig {
 
     @Bean
+    @Profile("!test")
     public FilterRegistrationBean<RequestValidationFilter> requestValidationFilter() {
         FilterRegistrationBean<RequestValidationFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new RequestValidationFilter());
