89c5934de34a868c7d94b4cf319c1adfa06d82c5
Vitthal Choudhari <112979693+vitthal1001@users.noreply.github.com>
2025-11-09 15:13:45 +0530

    feat(transaction): -fixed pr build and test err

--- src/test/java/ftms/svc/transactions/api/FtmsSvcTransactionsApiApplicationTests.java
+++ src/test/java/ftms/svc/transactions/api/FtmsSvcTransactionsApiApplicationTests.java
@@ -2,8 +2,10 @@ package ftms.svc.transactions.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FtmsSvcTransactionsApiApplicationTests {
 
 	@Test
